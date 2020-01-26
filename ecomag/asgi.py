60004f1ff40a86cbd13dcd596abb2d88269f6871
MTOUAA Mourad <mouradxmt@gmail.com>
2020-01-26 04:54:36 +0100

    Added some assets / Index file

--- ecomag/asgi.py
+++ ecomag/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ecomag project.
+ASGI config for ecommerce project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecomag.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
 
 application = get_asgi_application()
